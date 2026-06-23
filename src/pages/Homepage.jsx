ee24b7ebf9bb3c1e3c78f795dca2e092b37f2c68
rahul ray <rahulrayrazzzzz@gmail.com>
2026-06-23 18:56:08 +0530

    feat: Separated the navigation bar and created another jsx component for it

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,6 +3,7 @@ import { Link, NavLink } from 'react-router'; // Fixed import
 import { useDispatch, useSelector } from 'react-redux';
 import axiosClient from '../utils/axiosClient';
 import { logoutUser } from '../authSlice';
+import NavigationBar from './NavigationBar';
 
 function Homepage(){
 
@@ -19,6 +20,7 @@ function Homepage(){
         tag: 'all',
         status: 'all' 
       });
+
    async function deleteProblem(problemId){
           const isConfirmed=window.confirm("Are you sure you want to delete this problem?");
           if(isConfirmed){
@@ -74,10 +76,7 @@ function Homepage(){
 
 
 
-       const handleLogout = () => {
-           dispatch(logoutUser());
-           setSolvedProblems([]); // Clear solved problems on logout
-         };
+    
           
 
 
@@ -94,38 +93,7 @@ function Homepage(){
         <>
     <div className="min-h-screen bg-gradient-to-br from-slate-950 via-slate-900 to-slate-950 text-white">      
       {/* Navbar */}
-      <nav className="bg-slate-900/80 backdrop-blur-md border-b border-slate-800 px-8 py-4 flex justify-between items-center sticky top-0 z-50">
-        <NavLink
-          to="/"
-          className="text-3xl font-extrabold tracking-wide text-white hover:text-blue-400 transition"
-        >
-          CodeHub
-        </NavLink>
-         <div className='flex justify-between gap-5'>
-          {
-          user?.role==="admin"&&(
-             <Link to="/admin-panel">
-          <button className="px-5 py-2 bg-blue-600 hover:bg-blue-700 rounded-xl font-medium transition-all duration-300 shadow-lg hover:shadow-blue-500/20">Create Problem Section</button>
-          </Link>
-          )
-          }
-           <div className="relative group">
-          <button className="px-4 py-2 bg-slate-800 border border-slate-700 rounded-xl hover:bg-slate-700 transition font-medium"> 
-            {user?.firstName}
-          </button>
-
-          <div className="absolute top-12 right-0 w-40 bg-slate-900 border border-slate-700 shadow-2xl rounded-xl opacity-0 invisible group-hover:opacity-100 group-hover:visible transition duration-200 z-50">
-            <button
-              onClick={handleLogout}
-              className="w-full text-left px-4 py-3 text-red-400 hover:bg-slate-800 rounded-xl transition"
-            >
-              Logout
-            </button>
-          </div>
-        </div>
-      </div>
-       
-      </nav>
+      <NavigationBar user={user} setSolvedProblems={setSolvedProblems}/>
 
       {/* Main Content */}
       <div className="max-w-6xl mx-auto px-6 py-8">
